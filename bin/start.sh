c6bb08ddbc906c2e5155dee49f3d0d7f3ffed81d
Sverre Johansen <sverre.johansen@gmail.com>
2023-12-01 13:37:57 +0100

    Basic start script

---
+++ bin/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+
+# Define the file name
+root=$(git rev-parse --show-toplevel)
+path="$root/$year"
+filename="$path/day-$day.ts"
+
+if [ "$month" -eq 12 ] && [ "$day" -ge 1 ] && [ "$day" -le 24 ]; then
+    if [ ! -f "$filename" ]; then
+				mkdir -p "$path"
+        touch "$filename"
+    else
+        echo "File $filename already exists."
+    fi
+		open -a Emacs.app "$filename"
+else
+    echo "It's not between December 1 and 24."
+fi
