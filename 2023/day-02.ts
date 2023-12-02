dead67f605f53bef721a88a265759cd53d6f2d8d
Sverre Johansen <sverre.johansen@gmail.com>
2023-12-02 11:13:32 +0100

    whitespace

--- 2023/day-02.ts
+++ 2023/day-02.ts
@@ -1,10 +1,12 @@
 import { name } from "../lib/name";
 
 /*
---- Day 2: Cube Conundrum --- You're launched high into the atmosphere! The apex
-of your trajectory just barely reaches the surface of a large island floating in
-the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you
-don't see much snow. An Elf runs over to greet you.
+--- Day 2: Cube Conundrum ---
+
+You're launched high into the atmosphere! The apex of your trajectory just
+barely reaches the surface of a large island floating in the sky. You gently
+land in a fluffy pile of leaves. It's quite cold, but you don't see much
+snow. An Elf runs over to greet you.
 
 The Elf explains that you've arrived at Snow Island and apologizes for the lack
 of snow. He'll be happy to explain the situation, but it's a bit of a walk, so
